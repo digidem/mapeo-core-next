936a57956997de56de474aa35189bcc156333e26
sethvincent <sethvincent@gmail.com>
2022-06-08 17:19:24 -0700

    localpeers test

--- tests/localpeers.js
+++ tests/localpeers.js
@@ -1,7 +1,68 @@
 import { test } from 'brittle'
 
+import getPort from 'get-port'
+
 import { createCoreKeyPair, createIdentityKeys } from './helpers/index.js'
-import { MdnsPeerDiscovery } from '../lib/localpeers.js'
+import { LocalPeers, MdnsPeerDiscovery } from '../lib/localpeers.js'
+
+test('localpeers join & leave', async (t) => {
+	t.plan(8)
+
+	const keyPair = createCoreKeyPair('mdns-peer-discovery1')
+	const key = keyPair.publicKey.toString('hex')
+
+	const identity1 = createIdentityKeys()
+	const identityPublicKey1 = identity1.identityKeyPair.publicKey.toString('hex')
+
+	const identity2 = createIdentityKeys()
+	const identityPublicKey2 = identity2.identityKeyPair.publicKey.toString('hex')
+
+	const local1 = new LocalPeers({
+		name: 'mapeo',
+		port: await getPort(),
+		identityKeyPair: identity1.identityKeyPair
+	})
+
+	const local2 = new LocalPeers({
+		name: 'mapeo',
+		port: await getPort(),
+		identityKeyPair: identity2.identityKeyPair
+	})
+
+	const discover1 = local1.join(key)
+	const discover2 = local2.join(key)
+
+	let count = 0
+	discover1.on('connection', (connection, peer) => {
+		t.ok(peer.topic === key)
+		t.ok(Buffer.from(peer.topic, 'hex').equals(keyPair.publicKey))
+		t.ok(peer.identityPublicKey == identityPublicKey2)
+		t.ok(Buffer.from(peer.identityPublicKey, 'hex').equals(identity2.identityKeyPair.publicKey))
+		end()
+	})
+
+	discover2.on('connection', (connection, peer) => {
+		t.ok(peer.topic === key)
+		t.ok(Buffer.from(peer.topic, 'hex').equals(keyPair.publicKey))
+		t.ok(peer.identityPublicKey == identityPublicKey1)
+		t.ok(Buffer.from(peer.identityPublicKey, 'hex').equals(identity1.identityKeyPair.publicKey))
+		end()
+	})
+
+	function end () {
+		count++
+		if (count === 2) {
+			local1.leave(key)
+			local2.leave(key)
+
+			discover1.on('destroyed', () => {
+				discover2.on('destroyed', () => {
+					t.end()
+				})
+			})
+		}
+	}
+})
 
 test('mdns peer discovery: connect two peers', async (t) => {
 	t.plan(8)
@@ -19,14 +80,14 @@ test('mdns peer discovery: connect two peers', async (t) => {
 		name: 'mapeo',
 		topic: key,
 		identityKeyPair: identity1.identityKeyPair,
-		port: 4567,
+		port: await getPort(),
 	})
 
 	const discover2 = new MdnsPeerDiscovery({
 		name: 'mapeo',
 		topic: key,
 		identityKeyPair: identity2.identityKeyPair,
-		port: 5678,
+		port: await getPort(),
 	})
 
 	let count = 0
